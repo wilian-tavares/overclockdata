8bcc706d0c3b1ae41bc8453fae8c4f7f31a57f90
Wilian Tavares <wilian.tavares@souunit.com.br>
2024-08-25 15:16:53 -0300

    Feat: add funcionalidade Editar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import { useState, useEffect } from 'react'
-
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-
-
   const [cpu, setCpu] = useState("Ryzen 5 3600X");
   const [voltagem, setVoltagem] = useState("1.37");
   const [clock, setClock] = useState("3.800");
   const [temperatura, setTemperatura] = useState("75");
   const [estavel, setEstavel] = useState("");
 
-
-  const [perfil, setPerfil] = useState({
-    cpu: '',
-    voltagem: '',
-    clock: '',
-    temperatura: '',
-    estavel: '',
-
-  })
+  const [edit, setEdit] = useState(false);
+  const [editIndex, setEditIndex] = useState<number | null>(null);
 
   interface Perfil {
     cpu: string;
@@ -29,9 +19,7 @@ function App() {
     estavel: string;
   }
 
-
   const [allPerfil, setAllPerfil] = useState<Perfil[]>([]);
-
 
   useEffect(() => {
     const confSalvas = localStorage.getItem("overclock");
@@ -46,14 +34,19 @@ function App() {
     }
   }, [allPerfil]);
 
-
-
   function handlePerfil(cpu: string, voltagem: string, clock: string, temperatura: string, estavel: string) {
-
     const newPerfil: Perfil = { cpu, voltagem, clock, temperatura, estavel };
-    if (voltagem !== "" && clock !== "" && estavel !== "")
 
-    setAllPerfil(perfils => [...perfils, newPerfil]);
+    if (edit && editIndex !== null) {
+      const updatedPerfils = allPerfil.map((perfil, index) => 
+        index === editIndex ? newPerfil : perfil
+      );
+      setAllPerfil(updatedPerfils);
+      setEdit(false);
+      setEditIndex(null);
+    } else {
+      setAllPerfil(perfils => [...perfils, newPerfil]);
+    }
 
     setCpu("Ryzen 5 3600X");
     setVoltagem("");
@@ -62,73 +55,64 @@ function App() {
     setEstavel("");
   }
 
-  // function handleEdit(index: number){
-  //  // console.log(index);
-  //   const findPerfil = allPerfil.findIndex(perfil => perfil )
-  //   console.log(findPerfil)
+  function handleEdit(index: number) {
+    const updatePerfil = allPerfil[index];
+    setCpu(updatePerfil.cpu);
+    setVoltagem(updatePerfil.voltagem);
+    setClock(updatePerfil.clock);
+    setTemperatura(updatePerfil.temperatura);
+    setEstavel(updatePerfil.estavel);
 
-  
-  // }
+    setEdit(true);
+    setEditIndex(index);
+  }
 
   function handleDelete(index: number) {
     const updatedPerfil = allPerfil.filter((_, i) => i !== index);
     setAllPerfil(updatedPerfil);
   }
-  
 
   return (
     <div className='container'>
       <header>Overclock</header>
-
       <main>
         <h2 className='title'>Configuração Overclock</h2>
         <div className='area-cadastro'>
-
           <input className='input-text' type='input' value={cpu} onChange={(cpu) => setCpu(cpu.target.value)} placeholder='Informe Processador' />
           <input className='input-text' type='input' value={voltagem} onChange={(voltagem) => setVoltagem(voltagem.target.value)} placeholder='Informe Voltagem' />
           <input className='input-text' type='input' value={clock} onChange={(clock) => setClock(clock.target.value)} placeholder='Informe Clock Atual' />
           <input className='input-text' type='input' value={temperatura} onChange={(temperatura) => setTemperatura(temperatura.target.value)} placeholder='Informe Temperatura Máxima' />
 
-
           <p>O Overclock ficou estável?</p>
           <div className='shield-radio'>
-            <label >
+            <label>
               SIM
-              <input className='input-radio' type='radio' value="SIM" checked={estavel === 'SIM'} onChange={() => setEstavel("SIM")} placeholder='Informe Processador' />
+              <input className='input-radio' type='radio' value="SIM" checked={estavel === 'SIM'} onChange={() => setEstavel("SIM")} />
             </label>
-
             <label>
               NÃO
-              <input className='input-radio' type='radio' value="NÃO" checked={estavel === 'NÃO'} onChange={() => setEstavel("NÃO")} placeholder='Informe Processador' />
+              <input className='input-radio' type='radio' value="NÃO" checked={estavel === 'NÃO'} onChange={() => setEstavel("NÃO")} />
             </label>
-
           </div>
-          <button className='button-cadastrar' onClick={() => handlePerfil(cpu, voltagem, clock, temperatura, estavel)}>Cadastrar</button>
-
-
+          <button className='button-cadastrar' onClick={() => handlePerfil(cpu, voltagem, clock, temperatura, estavel)}>
+            {edit ? "Atualizar" : "Cadastrar"}
+          </button>
         </div>
 
         {allPerfil.map((perfil, index) => (
-          <>
-            <article key={index} className='lista-perfil'>
-              <p>{`CPU: ${perfil.cpu}`}</p>
-              <p>{`Volts: ${perfil.voltagem}`}</p>
-              <p>{`Clock: ${perfil.clock}Mhz`}</p>
-              <p>{`temperatura: ${perfil.temperatura}`}</p>
-              <p>{`Estável: ${perfil.estavel}`}</p>
-              {/* <button onClick={() => handleEdit(index)} className='button-editar'>Editar</button> */}
-              <button onClick={() => handleDelete(index)} className='button-apagar'>Apagar</button>
-
-            </article>
-          </>
+          <article key={index} className='lista-perfil'>
+            <p>{`CPU: ${perfil.cpu}`}</p>
+            <p>{`Volts: ${perfil.voltagem}`}</p>
+            <p>{`Clock: ${perfil.clock}`}</p>
+            <p>{`Temperatura: ${perfil.temperatura}`}</p>
+            <p>{`Estável: ${perfil.estavel}`}</p>
+            <button className='button-editar' onClick={() => handleEdit(index)}>Editar</button>
+            <button  className='button-apagar' onClick={() => handleDelete(index)}>Deletar</button>
+          </article>
         ))}
-
-
-
       </main>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
